7b23d210c80cf5e5e66cdd51f8c3d411c20bdb04
Joshua Garber <josh@jkgarber.com>
2025-07-30 23:44:19 +0200

    new concept

--- lieutenant.js
+++ lieutenant.js
@@ -1,19 +1,90 @@
-class LutHello extends HTMLElement {
+export class LieutenantBase extends HTMLElement {
     constructor() {
-        super();    
+        super();
     }
     connectedCallback() {
-        const styles = new CSSStyleSheet();
-        styles.replaceSync("span { font-weight: 800 }")
-        const shadow = this.attachShadow({ mode: "open" });
-        shadow.adoptedStyleSheets = [styles];
-        const text = this.getAttribute("data-text");
-        const span = document.createElement("span");
-        span.textContent = text;
-        shadow.appendChild(span);
+        this.showLoading();
+        this.styles = this.getAttribute("data-styles");
+        this.content = this.getAttribute("data-content");
+        this.render();
+    }
+    showLoading() {
+        this.clearContent();
+        this.appendChild(new LieutenantSpinner);
+    }
+    showError(message) {
+        this.clearContent();
+        this.appendChild(new LieutenantError(message));
+    }
+    async render() {
+        try {
+            const response = await fetch(this.styles);
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            const stylesText = await response.text();
+            const stylesheet = new CSSStyleSheet();
+            stylesheet.replaceSync(stylesText);
+            const shadow = this.attachShadow({ mode: "open" });
+            shadow.adoptedStyleSheets = [stylesheet];
+            this.renderContent();
+        }
+        catch(error) {
+            console.error(`Fetch problem: ${error.message}`);
+            this.showError("Something went wrong.");
+        }
+    }
+    async renderContent() {
+        try {
+            const response = await fetch(this.content);
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`);
+            }
+            this.removeLoading();
+            const htmlText = await response.text();
+            this.shadowRoot.innerHTML = htmlText;
+        }
+        catch(error) {
+            console.error(`Fetch problem: ${error.message}`);
+            this.showError("Something went wrong.");
+        }
+    }
+    clearContent() {
+        if (this.shadowRoot) {
+            this.shadowRoot.adoptedStyleSheets.length = 0;
+        }
+        this.childNodes.forEach((node) => {
+            node.remove();
+        });
+    }
+    removeLoading() {
+        this.querySelector("lieutenant-spinner").remove();
     }
 }
-customElements.define("lut-hello", LutHello);
+customElements.define("lieutenant-base", LieutenantBase);
 
 
-export { LutHello }
+export class LieutenantSpinner extends LieutenantBase {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.textContent = "Loading..."
+    }
+}
+customElements.define("lieutenant-spinner", LieutenantSpinner);
+
+
+export class LieutenantError extends LieutenantBase {
+    constructor(message) {
+        super()
+        this.message = message;
+    }
+    connectedCallback() {
+        this.textContent = this.message;
+    }
+}
+customElements.define("lieutenant-error", LieutenantError);
+
+export class BaseComp extends LieutenantBase {};
+customElements.define("base-comp", BaseComp);
